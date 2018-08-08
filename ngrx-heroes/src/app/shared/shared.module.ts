c492f2ff8afed3d3a63381efade14e6b55f46304
Adrian Ferreres Esteller <adrian.ferreres@censhare.com>
2018-08-08 20:04:19 +0200

    Add header
    1. Add web header
    2. Change the configuration for material icons
    3. Add Capitan America Shield image

--- ngrx-heroes/src/app/shared/shared.module.ts
+++ ngrx-heroes/src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { HeaderComponent } from './header.component';
 
+
 @NgModule({
-  imports: [ CommonModule, FormsModule],
+  imports: [ CommonModule, FormsModule, MatToolbarModule],
   declarations: [ HeaderComponent ],
   exports: [ HeaderComponent, FormsModule, CommonModule ]
 })
